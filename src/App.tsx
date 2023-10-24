474c174ab29895b7e50dd8887eebffb3a9013a49
ukarpenkov <truloveu@gmail.com>
2023-10-24 15:33:05 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Container } from 'components/Container'
+import { TheHeader } from 'components/TheHeader'
+
+function App() {
+  return (
+    <Container>
+      <TheHeader />
+    </Container>
+  )
+}
+
+export default App
