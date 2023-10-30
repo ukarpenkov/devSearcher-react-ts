4d518dfc975cbb04a264a66a529c25b1a010e78e
ukarpenkov <truloveu@gmail.com>
2023-10-30 14:50:42 +0300

    input event

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,6 +1,6 @@
 import { ReactComponent as SearchIcon } from 'assets/icon-search.svg'
 import { Button } from 'components/Button'
-import { useRef } from 'react'
+import { type } from 'os'
 
 import styles from './Search.module.scss'
 
@@ -9,16 +9,19 @@ interface SearchProps {
   onSubmit: (text: string) => void
 }
 
+type FormFields = {
+  username: HTMLInputElement
+}
+
 export const Search = ({ hasError, onSubmit }: SearchProps) => {
-  const searchRef = useRef<HTMLInputElement | null>(null)
-  const handleSubmit = (event: React.FormEvent) => {
+  const handleSubmit = (
+    event: React.FormEvent<HTMLFormElement & FormFields>
+  ) => {
     event.preventDefault()
-    const text = searchRef.current ? searchRef.current?.value : ''
+    const text = event.currentTarget.username.value
     if (text) {
       onSubmit(text)
-      if (searchRef.current) {
-        searchRef.current.value = ''
-      }
+      event.currentTarget.reset()
     }
   }
   return (
@@ -28,10 +31,10 @@ export const Search = ({ hasError, onSubmit }: SearchProps) => {
           <SearchIcon />
         </label>
         <input
-          ref={searchRef}
           type="text"
           className={styles.textField}
           id="search"
+          name="username"
           placeholder="Search GitHub username..."
         />
         {hasError && <div className={styles.error}>No result</div>}
