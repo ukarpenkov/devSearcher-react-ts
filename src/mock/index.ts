511603ad266e3d3eec209e31fcce35ffc32b8f2e
ukarpenkov <truloveu@gmail.com>
2023-10-31 14:56:12 +0300

    user card

---
+++ src/mock/index.ts
@@ -0,0 +1,17 @@
+import { LocalGithubUser } from 'types'
+
+export const defaultUser: LocalGithubUser = {
+  login: 'ukarpenkov',
+  id: 62246304,
+  avatar: 'https://avatars.githubusercontent.com/u/62246304?v=4',
+  name: 'Iurii Karpenkov',
+  company: 'null',
+  blog: '',
+  location: 'null',
+  bio: 'null',
+  twitter: 'null',
+  repos: 19,
+  followers: 0,
+  following: 0,
+  created: '2020-03-16T12:35:43Z',
+}
